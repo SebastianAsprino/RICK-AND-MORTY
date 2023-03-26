fbabfe0c8c46645c428613edbdfbded5e57bd8b1
Sebastian <asprinos@uninorte.edu.co>
2023-03-26 18:27:56 -0500

    la app con Bootstrap y react

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -1,13 +1,58 @@
+import React, {useEffect, useState} from 'react'
+import Navbar from "./components/Navbar";
+import Characters from './components/Characters';
+import Pagination from './components/Pagination';
+
+
+
+
+
 
 function App() {
-  return (
-    <nav className="navbar navbar-dark bg-dark" >
-      <div className="container">
-        <a className="navbar-brand text-uppercase" href= "/">rick and morty app</a>
-      </div>
-    </nav>
 
-    );
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data =>{
+        setCharacters(data.results)
+        setInfo(data.info)
+      } )
+      .catch(error => console.log(error))
+  };
+  const onPrevious =()=>{
+    fetchCharacters(info.prev)
+  }
+
+  const onNext =()=>{
+    fetchCharacters(info.next)
+  }
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  },[])
+  return (
+    <>
+      <Navbar brand= "rick and morty app" />
+      
+      <div className= "container mt-5">
+        <Pagination 
+          prev={info.prev} 
+          next={info.next} 
+          onPrevious={onPrevious} 
+          onNext={onNext}/>
+        <Characters characters={characters}/>
+        <Pagination 
+          prev={info.prev} 
+          next={info.next} 
+          onPrevious={onPrevious} 
+          onNext={onNext}/>
+      </div>
+    </>
+  );
 }
 
 export default App;
